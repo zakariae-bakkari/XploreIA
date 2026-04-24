a12dfa9335d9c47b01b4fd44211c89f2664a3a1d
Zakariae Bakkari <zakariabakkari2006@gmail.com>
2026-04-24 17:33:26 +0100

    feat: add environment configuration files and database connection settings

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'dbname' => 'xplore_ia',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4'
+    'host'     => getenv('DB_HOST')     ?: 'localhost',
+    'dbname'   => getenv('DB_NAME')     ?: 'xplore_ia',
+    'username' => getenv('DB_USERNAME') ?: 'root',
+    'password' => getenv('DB_PASSWORD') ?: '',
+    'charset'  => 'utf8mb4'
 ];
